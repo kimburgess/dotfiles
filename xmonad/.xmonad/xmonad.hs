713cb37fd00a98be83f45a6985c3d58ac4eab939
Kim Burgess <kim@acaprojects.com>
2018-01-31 15:54:21 +1000

    add suspend shortcut

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -4,7 +4,8 @@ import XMonad.Util.EZConfig(additionalKeys)
 modKey = mod4Mask  -- win key
 
 customKeys =
-  [ ((modKey, xK_x), spawn "dm-tool lock")
+  [ ((modKey, xK_x),       spawn "dm-tool lock")
+  , ((modKey, xK_Escape),  spawn "sudo pm-suspend")
   ]
 
 main = xmonad $ def
@@ -12,4 +13,3 @@ main = xmonad $ def
   , terminal = "alacritty"
   }
   `additionalKeys` customKeys
-
