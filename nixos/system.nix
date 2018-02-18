dcb674841429e663f16d343449becf96caee6362
Kim Burgess <kim@acaprojects.com>
2018-02-19 00:04:34 +1000

    add support for packages from the unstable channel

--- nixos/system.nix
+++ nixos/system.nix
@@ -8,4 +8,14 @@
   nix.gc.dates = "12:30";
 
   nixpkgs.config.allowUnfree = true;
+
+  # Enable pulling packages from the unstable branch via unstable.name
+  nixpkgs.config.packageOverrides = pkgs:
+    { unstable = import <nixos-unstable>
+        {
+            # pass the nixpkgs config to the unstable alias
+            # to ensure `allowUnfree` is propagated:
+            config = config.nixpkgs.config;
+        };
+    };
 }
