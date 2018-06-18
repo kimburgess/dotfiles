13e148a6018af767ad13d6753899fce94c8813b4
Kim Burgess <kim@acaprojects.com>
2018-06-18 11:57:34 +1000

    fix bundler version mismatch in vagrant

--- nixos/devtools.nix
+++ nixos/devtools.nix
@@ -8,6 +8,22 @@
 
   environment.systemPackages = with pkgs;
     let
+
+      # See https://github.com/NixOS/nixpkgs/issues/36880
+      vagrant = (import <nixpkgs> {
+        overlays = [
+          (self: super: {
+            bundler = super.bundler.overrideAttrs (old: {
+              name = "bundler-1.16.1";
+              src = super.fetchurl {
+                url = "https://rubygems.org/gems/bundler-1.16.1.gem";
+                sha256 = "1s2nq4qnffxg3kwrk7cnwxcvfihlhxm9absl2l6d3qckf3sy1f22";
+              };
+            });
+          })
+        ];
+      }).vagrant;
+
       editors = [ atom vim ];
       nix = [ cabal2nix nix-repl ];
       haskell = [ ghc cabal-install haskellPackages.ghc-mod ];
