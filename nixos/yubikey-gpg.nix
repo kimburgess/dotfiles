5442dbfb69a65a7a63d89cd502f40e56cd812c8c
Kim Burgess <kim@acaprojects.com>
2020-02-05 10:50:21 +1000

    fix broken yubikey conf

--- nixos/yubikey-gpg.nix
+++ nixos/yubikey-gpg.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.ssh.startAgent = false;
+  programs =
+    { ssh.startAgent = false;
+      gnupg.agent =
+        { enable = true;
+          enableSSHSupport = true;
+        };
+    };
 
   services.pcscd.enable = true;
+
+  nixpkgs.config.allowBroken = true;
 
   environment.systemPackages = with pkgs;
     [ gnupg
@@ -12,8 +20,9 @@
     ];
 
   environment.shellInit = ''
+    export GPG_TTY="$(tty)"
     gpg-connect-agent /bye
-    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
   '';
 
   services.udev.packages = [ pkgs.yubikey-personalization ];
